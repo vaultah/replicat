vaultah/replicat

commit 478b97ccfb2cf526769de6b5a347b5ecbaaa1aaf

Re-add -r/--repository to the help message (#13)
Update __main__.py

replicat/__main__.py
@@ -105,7 +105,7 @@ def main():
         args.repo[0], inherit_common=False, missing=_missing_backend_argument
     )
     main_parser = utils.make_main_parser(
-        utils.common_options_parser, backend_args_parser
+        utils.repository_parser, utils.common_options_parser, backend_args_parser
     )
     _, unknown = main_parser.parse_known_args(remaining_args, namespace=args)
 

commit 07daace610b6a29967f35a7fc0e37787d2b6be0a

Fix a regression introduced in https://github.com/vaultah/replicat/pull/13

replicat/__main__.py
@@ -107,7 +107,12 @@ def main():
     main_parser = utils.make_main_parser(
         utils.repository_parser, utils.common_options_parser, backend_args_parser
     )
-    _, unknown = main_parser.parse_known_args(remaining_args, namespace=args)
+    # NOTE: this will parse the -r/--repository CLI argument AGAIN. In theory, we
+    # should need to only parse remaining_args, as the argparse documentation makes
+    # an interesting claim that "if the target namespace already has an attribute set,
+    # the action default [i.e. -r/--repository's default] will not over write it".
+    # This claim turned out to be false. BPO-45235
+    _, unknown = main_parser.parse_known_args(namespace=args)
 
     if args.verbose >= 2:
         log_level = logging.DEBUG